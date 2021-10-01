42993916e2212d54f4ac816ac538e036b19f7269
Robert <strakowski.robert@gmail.com>
2021-10-01 12:04:12 +0200

    added test of buzzer

--- sysup_tank_v1/src/main.cpp
+++ sysup_tank_v1/src/main.cpp
@@ -8,11 +8,13 @@ int main() {
   PORTH = 0x08;
   DDRE |= 0x08;
   PORTE &= ~0x08;
+  DDRB |= 0x20;
+  PORTB = 0x20;
   while(1){
     _delay_ms(400);
     PORTH ^= 0x08;
     PORTE ^= 0x08;
-
+    PORTB ^= 0x20;
   }
 
   return 0;
